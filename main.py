33b01852da030afe6e1bec3562b3b885c35cb019
nynhle <nynhle90@gmail.com>
2015-10-20 23:10:59 -1000

    Now crawling cs294 page and stops. Site has 327 subpages, seems strange. Duplicates may exist. Need to investigate further.

---
+++ main.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+class Scraper(object):
+	
+	def __init__(self, starting_domain, starting_subpage):
+		self.starting_domain = starting_domain
+		self.starting_subpage = starting_subpage
+		self.visited_links = []
+		self.unvisited_links = []
+		self.unvisited_links.append(starting_subpage)
+
+	def get_webpage(self, url):
+		return urllib2.urlopen(url)
+
+	def scrape_site_extract_links(self):
+		next_subpage = self.unvisited_links.pop(0)
+		next_site = self.starting_domain + next_subpage
+		print next_site
+		site = urllib2.urlopen(next_site)
+		tree = BeautifulSoup(site, 'html.parser')
+		self.visited_links.append(str(next_subpage))
+		for linknode in tree.find_all('a'):
+			link = str(linknode.get('href'))
+			if link not in self.visited_links:
+				if link.startswith('/cs294_fall2015'):
+					self.unvisited_links.append(link)
+			else:
+				pass
+	
+
+	def Scrape(self):
+		while len(self.unvisited_links) > 0:
+			print "Scraping..."
+			self.scrape_site_extract_links()
+			print "One subpage scraped!"
+			print "Scraped subpages: " + str(len(self.visited_links))
+			print "Subpages to go: " + str(len(self.unvisited_links))
+
+scraper = Scraper('http://bruab.github.io', '/cs294_fall2015/index.html')
+scraper.Scrape()
+
