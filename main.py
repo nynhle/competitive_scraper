5b4f57be34c26acab4d20ce6d0ba9009f7852438
nynhle <nynhle90@gmail.com>
2015-10-22 11:22:45 -1000

    Refactor using only sets. Possible to use selection too but this one is probably cleaner.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sets import Set
 from bs4 import BeautifulSoup
 import urllib2
 
@@ -6,27 +7,25 @@ class Crawler(object):
 	def __init__(self, starting_domain, starting_subpage):
 		self.starting_domain = starting_domain
 		self.starting_subpage = starting_subpage
-		self.visited_links = []
-		self.unvisited_links = []
-		self.unvisited_links.append(starting_subpage)
+		self.visited_links = Set()
+		self.unvisited_links = Set()
+		self.unvisited_links.add(starting_subpage)
 
 	def get_webpage(self, url):
 		return urllib2.urlopen(url)
 
 	def scrape_site_extract_links(self):	
-		next_subpage = self.unvisited_links.pop(0)
+		next_subpage = self.unvisited_links.pop()
 		next_site = self.starting_domain + next_subpage
 		print next_site
 		site = urllib2.urlopen(next_site)
 		tree = BeautifulSoup(site, 'html.parser')
-		self.visited_links.append(str(next_subpage))
+		self.visited_links.add(str(next_subpage))
 		for linknode in tree.find_all('a'):
 			link = str(linknode.get('href'))
 			if link.startswith('/cs294_fall2015'):
 				if link not in self.visited_links:
-					self.unvisited_links.append(link)
-					self.unvisited_links = list(set(self.unvisited_links))
-					
+					self.unvisited_links.add(link)
 	
 
 	def Crawl(self):
