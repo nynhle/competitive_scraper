136172727bccdd75f7e177cfa5026d68fb1b6731
nynhle <nynhle90@gmail.com>
2015-10-22 13:46:02 -1000

    Refactor crawler class into separate file + user is prompted to choose domain and starting subpage

--- main.py
+++ main.py
@@ -1,41 +1,11 @@
-from sets import Set
-from bs4 import BeautifulSoup
-import urllib2
+import Crawler
 
-class Crawler(object):
-	
-	def __init__(self, starting_domain, starting_subpage):
-		self.starting_domain = starting_domain
-		self.starting_subpage = starting_subpage
-		self.visited_links = Set()
-		self.unvisited_links = Set()
-		self.unvisited_links.add(starting_subpage)
+print "Welcome to competitive scraper v. 0.01"
+print "Currently only supporting website crawling..."
 
-	def get_webpage(self, url):
-		return urllib2.urlopen(url)
+domain = raw_input("\nWhat domain to limit to? ")
+subsite = raw_input("Which subpage to start from? (blank if same as domain) ")
 
-	def scrape_site_extract_links(self):	
-		next_subpage = self.unvisited_links.pop()
-		next_site = self.starting_domain + next_subpage
-		print next_site
-		site = urllib2.urlopen(next_site)
-		tree = BeautifulSoup(site, 'html.parser')
-		self.visited_links.add(str(next_subpage))
-		for linknode in tree.find_all('a'):
-			link = str(linknode.get('href'))
-			if link.startswith('/cs294_fall2015'):
-				if link not in self.visited_links:
-					self.unvisited_links.add(link)
-	
-
-	def Crawl(self):
-		while len(self.unvisited_links) > 0:
-			print "Crawling..."
-			self.scrape_site_extract_links()
-			print "One subpage crawled!"
-			print "Crawled subpages: " + str(len(self.visited_links))
-			print "Subpages to go: " + str(len(self.unvisited_links))
-
-crawler = Crawler('http://bruab.github.io', '/cs294_fall2015/index.html')
+crawler = Crawler.Crawler(domain, subsite)
 crawler.Crawl()
 
