e67fc48467576207561f12cf0923c327aa64295d
nynhle <nynhle90@gmail.com>
2015-10-22 17:16:08 -1000

    created directories for tests and src

---
+++ tests/crawlertests.py
@@ -0,0 +1,14 @@
+import unittest
+import main
+
+class TestCrawler(unittest.TestCase):
+	def __init__(self, *args, **kwargs):
+		super(TestCrawler, self).__init__(*args, **kwargs)
+		self.crawler = main.Crawler('http://bruab.github.io', '/cs294_fall2015/')
+
+	def test_no_link_duplicates(self):
+		self.assertEqual(self.crawler.visited_links, list(set(self.crawler.visited_links)))
+
+
+if __name__ == '__main__':
+	unittest.main()
