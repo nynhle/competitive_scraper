b60013d933f8aeed5a9812baae4e18fa4ee3a581
nynhle <nynhle90@gmail.com>
2015-11-11 13:21:20 -1000

    Updated main to act as controller for now.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src import Crawler
+from src import Scraper
 
 print "Welcome to competitive scraper v. 0.01"
 print "Currently only supporting website crawling..."
@@ -7,5 +8,7 @@ domain = raw_input("\nWhat domain to limit to? ")
 subsite = raw_input("Which subpage to start from? (blank if same as domain) ")
 
 crawler = Crawler.Crawler(domain, subsite)
-crawler.Crawl()
+url_list = crawler.Crawl()
+scraper = Scraper.Scraper(url_list)
+scraper.Scrape()
 
