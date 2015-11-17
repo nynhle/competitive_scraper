13ecfb885c4e3315f40a3d8506983a00a0c7acb3
nynhle <nynhle90@gmail.com>
2015-11-16 18:04:07 -1000

    Updated the way the server is started

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import unittest
 import os
 from src import Scraper
 
-os.system('python server.py')
+os.system('python -m SimpleHTTPServer 8000')
 
 class TestScraper(unittest.TestCase):
 	scraper = Scraper.Scraper('http://localhost/testpages/first_version.html')
