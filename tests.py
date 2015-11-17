53be3c541d5509a1c20fc474fa742973d1bb69ac
nynhle <nynhle90@gmail.com>
2015-11-16 18:15:48 -1000

    Seems like spawnl should be used anyways..

--- tests.py
+++ tests.py
@@ -2,8 +2,7 @@ import unittest
 import os
 from src import Scraper
 
-os.system('python -m SimpleHTTPServer 8000')
-
+os.spawnl(os.P_NOWAIT, 'python -m SimpleHTTPServer 8000')
 class TestScraper(unittest.TestCase):
 	scraper = Scraper.Scraper('http://localhost:8000/testpages/first_version.html')
 	scraper.Scrape()
