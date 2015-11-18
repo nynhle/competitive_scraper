fcad22076980d73fabffc547ea23f30b0df932ca
nynhle <nynhle90@gmail.com>
2015-11-18 00:43:19 -1000

    Added comment about how to run the test

--- tests.py
+++ tests.py
@@ -3,6 +3,11 @@ import os
 from src import Crawler
 from src import Scraper
 
+# Before running the tests, start a SimpleHTTPServer from the 
+# competitive scraper directory by running:
+# 'python -m SimpleHTTPServer 8000
+# TODO: Make this server start when the tests are run.
+
 class TestScraper(unittest.TestCase):
 	crawler = Crawler.Crawler('http://localhost:8000/testpages/first_version.html')
 	crawler.Crawl()
