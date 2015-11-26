a5c0692d4f18082bafda1b4615f265ab33284370
nynhle <nynhle90@gmail.com>
2015-11-25 16:54:09 -1000

    Menu added

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from src import Crawler
 from src import Scraper
 from src import Comparer
 
-
 def print_menu():
 	print '\nMenu'
 	print '1. Manually scrape a website.'
@@ -22,4 +21,23 @@ def manually_scrape():
 	counted_files = comparer.compare()
 
 	print "Domain scraped. " + str(counted_files) + " changes since last run."
+
+user_input = ""
+
+while user_input != '4':
+	print_menu()
+	user_input = raw_input('\nWhich action to take?')
+	
+	if user_input == '1':
+		manually_scrape()
+	elif user_input == '2':
+		print 'Not implemented yet'
+	elif user_input == '3':
+		print 'Not implemented yet'
+	elif user_input == '4':
+		break
+	else:
+		print 'Not a valid input, please try again.'
+
+
 
