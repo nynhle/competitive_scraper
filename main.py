2e6ef3fe165edb4d1ac157182db5a9d703e722f5
nynhle <nynhle90@gmail.com>
2015-11-25 16:47:37 -1000

    Refactored into a manual scraping method

--- main.py
+++ main.py
@@ -2,19 +2,6 @@ from src import Crawler
 from src import Scraper
 from src import Comparer
 
-print "Welcome to competitive scraper v. 0.01"
-
-domain = raw_input("\nWhat domain to limit to? ")
-subsite = raw_input("Which subpage to start from? (blank if same as domain) ")
-
-crawler = Crawler.Crawler(domain, subsite)
-url_list = crawler.Crawl()
-scraper = Scraper.Scraper(url_list)
-scraper.Scrape()
-comparer = Comparer.Comparer()
-counted_files = comparer.compare()
-
-print "Domain scraped. " + str(counted_files) + " changes since last run."
 
 def print_menu():
 	print '\nMenu'
@@ -23,4 +10,16 @@ def print_menu():
 	print '3. Delete saved data'
 	print '4. Exit' 
 
+def manually_scrape():
+	domain = raw_input("\nWhat domain to limit to? ")
+	subsite = raw_input("Which subpage to start from? (blank if same as domain) ")
+
+	crawler = Crawler.Crawler(domain, subsite)
+	url_list = crawler.Crawl()
+	scraper = Scraper.Scraper(url_list)
+	scraper.Scrape()
+	comparer = Comparer.Comparer()
+	counted_files = comparer.compare()
+
+	print "Domain scraped. " + str(counted_files) + " changes since last run."
 
