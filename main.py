e7ac6c4560347561b325a447304d8228b2eebce7
nynhle <nynhle90@gmail.com>
2015-11-25 16:39:31 -1000

    Doing some user output from main. Will change with better architecture

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from src import Crawler
 from src import Scraper
+from src import Comparer
 
 print "Welcome to competitive scraper v. 0.01"
-print "Currently only supporting website crawling..."
 
 domain = raw_input("\nWhat domain to limit to? ")
 subsite = raw_input("Which subpage to start from? (blank if same as domain) ")
@@ -11,4 +11,9 @@ crawler = Crawler.Crawler(domain, subsite)
 url_list = crawler.Crawl()
 scraper = Scraper.Scraper(url_list)
 scraper.Scrape()
+comparer = Comparer.Comparer()
+counted_files = comparer.compare()
+
+print "Domain scraped. Found " + str(counted_files) + " since last run."
+
 
