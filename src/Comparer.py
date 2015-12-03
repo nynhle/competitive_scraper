b2b60e0bad65b0c707359f949a4ce8dd2b3d4737
nynhle <nynhle90@gmail.com>
2015-12-02 21:24:13 -1000

    Finally it seems like the comparer is working, yay

--- src/Comparer.py
+++ src/Comparer.py
@@ -46,36 +46,52 @@ class Comparer(object):
 		return result
 
 	def parse_index_url_file(self):
-		url_list_file = []
+		url_list_file = set()
 		index_file = self.return_index()
 		for line in index_file:
 			if '#' in line:
 				url = UrlFile(self.get_line_key(line), self.get_line_url(line))
-				url_list_file.append(url)
+				url_list_file.add(url)
 
 		return url_list_file
 
 	def parse_old_index_url_file(self):
-		old_list_file = []
+		old_list_file = set()
 		old_file = self.return_old_index()
 		for line in old_file:
 			if '#' in line:
 				url = UrlFile(self.get_line_key(line), self.get_line_url(line))
-				old_list_file.append(url)
+				old_list_file.add(url)
 
 		return old_list_file
 	
-	# TODO: Add logic for removed or added pages.
 	def get_matched_pairs(self):
 		new_urls = self.parse_index_url_file()
+		matched_new_urls = set()
+		unmatched_new_urls = set()
 		old_urls = self.parse_old_index_url_file()
-		pair_list = []
-		
+		matched_old_urls = set()
+		unmatched_old_urls = set()
+		pair_list = set()
+
 		for url in new_urls:
 			for old_url in old_urls:
 				if url.url == old_url.url:
 					pair = FilePair(url, old_url)
-					pair_list.append(pair)
+					pair_list.add(pair)
+					matched_new_urls.add(url.url)
+					matched_old_urls.add(old_url.url)
+		
+		for url in new_urls:
+			if url.url not in matched_new_urls:
+				pair = NoOldFilePair(url)
+				pair_list.add(pair)
+
+		for url in old_urls:
+			if url.url not in matched_old_urls:
+				pair = NoNewFilePair(url)
+				pair_list.add(pair)
+
 		return pair_list
 
 	def compare(self):
@@ -89,20 +105,17 @@ class Comparer(object):
 			index_file = open('data/changes/' + time + '/index.txt', 'a')
 			log = open('data/changes/' + time + '/' + str(filechange_index) + '.txt', 'w').close()
 			log = open('data/changes/' + time + '/' + str(filechange_index) + '.txt', 'a')
-			new_site = open('data/webpages/index/' + str(pair.new.key) + '.txt', 'r')
-			old_site = open('data/webpages/old/' + str(pair.old.key) + '.txt', 'r')
-			diff = difflib.context_diff(new_site.readlines(), old_site.readlines())
-			delta = ''.join(diff)
-			log.write('=====CHANGES FOR URL: ' + pair.new.url + '=====\n\n')
-			log.write(delta)
+			pair.set_new_webpage()
+			pair.set_old_webpage()
+			pair.calculate_delta()
+			log.write('=====CHANGES FOR URL: ' + pair.url + '=====\n\n')
+			log.write(pair.return_delta())
 			log.close()
 			# For test purposes:
 			test_log = open('data/changes.txt', 'w')
-			test_log.write(delta)
+			test_log.write(pair.return_delta())
 			test_log.close()
-			new_site.close()
-			old_site.close()
-			index_file.write(str(filechange_index) + '  ====>  ' + pair.new.url)
+			index_file.write(str(filechange_index) + '  ====>  ' + pair.url)
 			filechange_index += 1
 			
 
@@ -122,23 +135,40 @@ class FilePair(object):
 	def __init__(self, new, old):
 		self.new = new
 		self.old = old
-		self.set_new_page()
-		self.set_old_page()
-		self.compute_diff()
+		self.url = new.url
 
-	def set_new_page(self):
-		page_file = open('data/webpages/index/' + str(self.new.key) + '.txt', 'r')
-		self.new_page = page_file.readlines()
-		page_file.close()
+	def calculate_delta(self):
+		diff = difflib.context_diff(self.old_site, self.new_site)
+		delta = ''.join(diff)
+		self.delta = delta
+
+	def return_delta(self):
+		self.calculate_delta()
+		return self.delta
 	
-	def set_old_page(self):
-		page_file = open('data/webpages/old/' + str(self.old.key) + '.txt', 'r')
-		self.old_page = page_file.readlines()
-		page_file.close()
+	def set_new_webpage(self):
+		site = open('data/webpages/index/' + str(self.new.key) + '.txt', 'r')
+		self.new_site = site.readlines()
+		site.close()
 
-	def compute_diff(self):
-		self.diff = difflib.context_diff(self.new_page, self.old_page)
+	def set_old_webpage(self):
+		site = open('data/webpages/index/' + str(self.old.key) + '.txt', 'r')
+		self.old_site = site.readlines()
+		site.close()
 
-	def return_diff(self):
-		return ''.join(self.diff)
+class NoOldFilePair(FilePair):
+	def __init__(self, new):
+		self.new = new
+		self.url = new.url
+
+	def set_old_webpage(self):
+		self.old_site = ''
+
+class NoNewFilePair(FilePair):
+	def __init__(self, old):
+		self.old = old
+		self.url = old.url
+
+	def set_new_webpage(self):
+		self.new_site = ''
 
